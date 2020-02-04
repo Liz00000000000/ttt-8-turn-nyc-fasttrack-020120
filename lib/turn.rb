82a40430ac52a035b9d8a11933ad6a3b4a002a05
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-04 17:13:30 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,10 @@
+require_relative "../bin/turn.rb"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn(board)
+  puts "Welcome to Tic Tac Toe!"
+  puts [board]
+  puts "Please enter 1-9:"
+  gets.moves"../bin/turn.rb"
+
+end
